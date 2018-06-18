a1f513cd3fb6ea30451ef33cff634275f041cbb3
carlcorder <corder.carl@gmail.com>
2018-06-18 13:08:26 -0500

    using pug templates with html textarea

--- randomnotes.js
+++ randomnotes.js
@@ -1,16 +1,50 @@
+const { DATABASE_URL } = require('./config.json');
 const { Client } = require('pg');
 
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 5000;
+
+// app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.set('view engine', 'pug');
+app.get('/', (req, res) => {
+	getNote(res);
+});
+
+app.post('/', (req, res) => {
+	let randomnotes = req.body.content;
+	console.log(randomnotes);
+	insertNote(randomnotes);
+	res.redirect('back');
+	});
+
+app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
+
 const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true,
+    connectionString: process.env.DATABASE_URL || DATABASE_URL,
+    ssl: true,
 });
 
-client.connect();
-
-client.query('SELECT * FROM notes;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-  client.end();
+client.connect().then(() => {
+	client.query(`CREATE TABLE IF NOT EXISTS notes (id SERIAL PRIMARY KEY, content TEXT NOT NULL);`, (err, result) => {
+		err ? console.error(err) : console.log("");
+	});
 });
+
+let insertNote = (content) => {
+    client.query(`INSERT INTO notes (content) VALUES ('${content}');`, (err, result) => {
+        err ? console.log(err) : console.log(result);
+    });
+}
+
+let getNote = (res) => {
+	client.query(`SELECT content FROM notes ORDER BY id DESC LIMIT 1;`)
+		.then(result => {
+			let note = result.rows[0];
+			note ? res.render('index', {note: note['content']}) : res.render('index');
+		})
+		.catch(err => console.error(err));
+}
--- randomnotes.sql
+++ randomnotes.sql
@@ -1,5 +1,9 @@
-CREATE TABLE notes (id SERIAL PRIMARY KEY, content TEXT NOT NULL);
+CREATE TABLE IF NOT EXISTS notes (id SERIAL PRIMARY KEY, content TEXT NOT NULL);
 
 INSERT INTO notes (content) VALUES ('My first note.');
 INSERT INTO notes (content) VALUES ('My second note.');
 INSERT INTO notes (content) VALUES ('My third note.');
+
+SELECT content FROM notes ORDER BY id DESC LIMIT 1;
+
+DROP TABLE notes;

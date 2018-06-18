e20b542c2f92ed52802c17789a132829af25d780
carlcorder <corder.carl@gmail.com>
2018-06-17 21:57:35 -0500

    ps client

---
+++ randomnotes.sql
@@ -0,0 +1,5 @@
+CREATE TABLE notes (id SERIAL PRIMARY KEY, content TEXT NOT NULL);
+
+INSERT INTO notes (content) VALUES ('My first note.');
+INSERT INTO notes (content) VALUES ('My second note.');
+INSERT INTO notes (content) VALUES ('My third note.');
